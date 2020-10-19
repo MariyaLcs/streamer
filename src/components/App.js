babfa41565ab575ea3578deeab1f17053f4b00f3
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-19 12:27:39 +0100

    wiring up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
