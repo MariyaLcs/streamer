d2bf9d05c0520f40fbb7f3044bbce3853ae3f6ed
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-22 14:54:51 +0100

    showing Errors on Touch and highlighting Errored fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,14 +2,25 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        <div>{this.renderError(meta)}</div>
       </div>
     );
-  }
+  };
   onSubmit(formValues) {
     console.log(formValues);
   }
@@ -17,7 +28,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
@@ -37,7 +48,7 @@ const validate = (formValues) => {
     errors.title = "You must enter a title";
   }
   if (!formValues.description) {
-    errors.title = "You must enter a description";
+    errors.description = "You must enter a description";
   }
   return errors;
 };
