5a21276387604e62b85d45daa6b7711f76ade56f
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-27 15:38:21 +0000

    edit Form submission, adding patch req

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream, editStream } from "../../actions";
@@ -8,7 +9,7 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
   onSubmit = (formValues) => {
-    console.log(formValues);
+    this.props.editStream(this.props.match.params.id, formValues)
   };
   render() {
     if (!this.props.stream) {
@@ -17,13 +18,18 @@ class StreamEdit extends React.Component {
     return (
       <div>
         <h3>Edit a Stream</h3>
-        <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit}/>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
       </div>
-    )
+    );
   }
 }
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
