4908036107882d14563391927bd1af633613bf11
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-28 08:56:10 +0000

    creating a portal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Modal from "../Modal"
 
 const StreamDelete = () => {
-  return <div className="">StreamDelete</div>;
+  return (<div className="">StreamDelete <Modal/></div>);
 };
 
 export default StreamDelete;
